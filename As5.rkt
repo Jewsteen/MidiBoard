7962c85e9c59d2ab47ec660fceb99e16945d3b6b
unknown <rpremi@hotmail.com>
2017-11-06 11:51:43 -0800

    test

--- As5.rkt
+++ As5.rkt
@@ -2,4 +2,6 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname As5) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
+(+ 2 2)
+
 (+ 1 1)
