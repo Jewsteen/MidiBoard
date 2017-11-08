ed8e1057cbd831cbc00609034d70be9fb5da21bd
Jewsteen <Justin1.wills@gmail.com>
2017-11-08 11:52:00 -0800

    data definition

--- MidiBoardMain.rkt
+++ MidiBoardMain.rkt
@@ -1,3 +1,10 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname MidiBoardMain) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;md is a world state
+;md includes
+;volume
+;playing?
+;pitch
+;layoutnum
+;kitnum
