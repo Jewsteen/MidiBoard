24f3ad1df4f9184c8b2476f1472068f59247a80f
Jewsteen <Justin1.wills@gmail.com>
2017-11-10 11:20:36 -0800

    final data definition for worldstate 'ms'

--- MidiBoardMain.rkt
+++ MidiBoardMain.rkt
@@ -1,10 +1,14 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname MidiBoardMain) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;md is a world state
-;md includes
-;volume
-;playing?
-;pitch
-;layoutnum
-;kitnum
+;ms is a world state
+;ms includes
+;volume - decimal 0.0 - 1.0
+;pressed? - list of booleans that indicate what keys are being pressed
+;octave - number between 0 and 8 that indicate the midi number for each key
+;kit - a structure that indicates path and integer value (make-kit "vgame" 60)
+;record? - boolean that indicates if the sounds are being recorded
+;recordlength - frames long of the recording
+;rate - a multiplier of how fast soundws are played
+;tempo - how fast the metronome moves in bpm (needs to convert from frames per second)
+(define-struct ms (volume pressed? octave kit record? recordlength rate tempo))
