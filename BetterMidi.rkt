8197a4a27f92b1ac9a20264e500e5a17db98ff37
unknown <rpremi@hotmail.com>
2017-12-01 05:31:05 -0800

    Added Tempo and Comments

--- BetterMidi.rkt
+++ BetterMidi.rkt
@@ -23,6 +23,8 @@
 ;kitnumber - a whole number that deterimess the number
 (define-struct kit (kitname kitnum))
 
+(define-struct pstate (p? frames))
+
 ;test variables defined for you guessed it, testing
 (define testkit (make-kit "vgame" 50))
 (define test (make-ms .8 (make-list 52 #f) (cons (make-list 4 (silence 1) ) '()) 6 testkit #f (silence 1) 0 133))
@@ -56,7 +58,6 @@
 ;colorKey outputs a rectangle color based on what it's original color was
 ;and whether it's currently pressed or not
 ; WorldState(ms) number number -> string
-
 (define (colorKey ws index col)
   (cond
     [(equal? 1 col ) 
@@ -72,12 +73,15 @@
     [else "green"] 
     ))
 
-
+;NoteIndex outputs the index of the onscreen note based on
+;what current note is selected.
+;Number Number -> Number
 (define (NoteIndex cNote pos )
   (+ (* 4 (floor (/ cNote 4))) pos))
 
-
-
+;colorsect outputs an image to show differing UI elements as colorful rectangle(s)
+;depending on what rectangle is 
+;WorldState Number Number -> Image
 (define (colorSect ws type rect)
 (cond
   [(equal? type 0)
@@ -105,9 +109,16 @@
          )
      
      ]
-  
+  [(equal? type 3)
+   (beside
+    (rectangle (* 100 (ms-volume ws)) 50 "solid" "green")
+    (rectangle (- 100 (* 100 (ms-volume ws))) 50 "solid" "red"))
+  ]
   ))
 
+
+;oc outputs an key(image) based on a name and position
+;String Number WorldState -> Image
 (define (oc str num ms)
    (overlay/xy (rectangle 67.5 210.825 "outline" (colorKey ms num 1)) 20 140 (text str 40 "black")))
 
@@ -153,8 +164,9 @@
 
                       )
               ))
-    200
+    150
     -100
+    
     (overlay/xy
      (beside
                 (text  "Volume: "  24 "White" )
@@ -162,21 +174,24 @@
                 (text (string-append " | " (number->string (* 100 (ms-volume ms))) "% ") 24 "White" )
                
           )
-     -75
+     0
      0
    (overlay/xy
      (beside
                  (text  "Octave: "  24 "White" )
                  (colorSect ms 1 1)
-                 (text (string-append " | " (number->string  (ms-octave ms)) " NoteNum " (number->string (length (ms-pressed? ms)) )) 24 "White" )
+                 (text (string-append " | " (number->string  (ms-octave ms)) "    Tempo: " (number->string (ms-tempo ms))) 24 "White" )
+
 
              
           )
-     -350
+     -300
      100
      (beside
-                       (rectangle 50 50 "solid" "black")
 
+                   (text  (string-append "M: " (number->string (+(floor(/(ms-rate ms)4))1) )) 24 "White" )
+
+      (rectangle 50 50 "solid" "black")
                 (colorSect ms 2 0)
                  (rectangle 50 50 "solid" "black")
                   (colorSect ms 2 1)
@@ -198,10 +213,6 @@
 
 ;=======================================================
 
-
-(define one 44100)
-
-
 ;octavator is a function that converts a number of an octave into the various midi numbers
 ;number -> list-of-midi
 (define (octavator trace) 
@@ -216,18 +227,11 @@
 ;selector is a helper function that calls a specific element in an octave from a list of numbers
 ;worldstate index -> midi
 (define (selector world index)
-  (list-ref (octavator (ms-octave world)) index)
-)
-
+  (list-ref (octavator (ms-octave world)) index))
 ;(check-expect (selector test 0) 0)
-;(check-expect (selector test 11) 11)
-
 
 ; Creates a new world state with the opposite boolean value at a given index in 
 ; WorldState number -> Worldstate
-
-;(define-struct ms (volume pressed? Plength octave kit record? recordlength rate tempo))
-
 
 ;oppBool creates a new pressed? list and outputs a new world state
 ;with an opposite boolean value at the given index
@@ -237,8 +241,6 @@
             (list-set (ms-pressed? ws) (+ index (* 13 (ms-rate ws))) (not (IndR ws index)) )
   (ms-Plength ws) (ms-octave ws) (ms-kit ws) (ms-record? ws) (ms-recordlength ws) (ms-rate ws) (ms-tempo ws))
   )
-
-;(synth-note (kit-kitname (ms-kit world)) (kit-kitnum (ms-kit world)) (selector world 0) one)
 
 ;Octav takes in a worldstate and a number
 ;then changes or doesn't change the octave based on the current world
@@ -272,6 +274,22 @@
              (ms-pressed? world) (ms-Plength world) (ms-octave world) (ms-kit world) (ms-record? world) (ms-recordlength world) (ms-rate world) (ms-tempo world))]
 ))
 
+;Vol takes in a worldstate and a number
+;then changes or doesn't change the volume based on the current world
+;eithering increasing or decreasing the volume
+;WorldState(ms) Number -> WorldState(ms)
+(define (temp world funct)
+  (cond
+    [(and (equal? 200 (ms-tempo world))(equal? funct 1)) world]
+    [(and (equal? 0 (ms-tempo world))(equal? funct 0)) world]
+    [ else  (make-ms (ms-volume world) (ms-pressed? world) (ms-Plength world) (ms-octave world) (ms-kit world) (ms-record? world) (ms-recordlength world) (ms-rate world)
+             (cond
+                    [(equal? funct 0) (- (ms-tempo world )1)]
+                    [else (+ (ms-tempo world) 1)]
+                     )
+             )]
+))
+
 ;Mchg outputs a worldstate with the correct current note and a new note to the song if necessary
 ;WorldState(ms) boolean -> WorldState(ms)
 (define (Mchg ws next?)
@@ -300,7 +318,7 @@
 ;synthcreation is a function that takes in a world state and a list and makes a synthnote
 ;worldstate boolean number -> synthnote
 (define (synthcreation world i)
-  (synth-note (kit-kitname (ms-kit world)) (kit-kitnum (ms-kit world)) (+ (* 12 (+ 1 (ms-octave world))) i) (round (* 48000 (/ 60 (ms-tempo world)))))
+  (rs-scale (ms-volume world) (synth-note (kit-kitname (ms-kit world)) (kit-kitnum (ms-kit world)) (+ (* 12 (+ 1 (ms-octave world))) i) (round (* 48000 (/ 60 (ms-tempo world))))))
   
   )
 ;(check-expect (synthcreation test 2) (synth-note "vgame" 50 86 44100))
@@ -332,9 +350,7 @@
 
 (define (SaveList ws)
  (make-ms (ms-volume ws) (ms-pressed? ws) (ms-Plength ws) (ms-octave ws) (ms-kit ws) (rs-append* (LtRS ws (ms-pressed? ws) 0) )  (ms-recordlength ws) (ms-rate ws) (ms-tempo ws)) )
- 
 ;(check-expect (soundconverter test (list #f #f #t #t #t #f #f #f #f #f #f #f #f) 0) (list (synth-note "vgame" 50 86 (round (* 48000(/ 60 (ms-tempo test))))) (synth-note "vgame" 50 87 (round (* 48000(/ 60 (ms-tempo test))))) (synth-note "vgame" 50 88 (round (* 48000(/ 60 (ms-tempo test)))))))
-
 
 ;keytracker is a function that maps midi notes to keys based on the octave and produces a make-tone
 ;worldstate key -> rsound
@@ -357,6 +373,8 @@
         [(key=? "]" key) (octav world 1) ]
         [(key=? "-" key) (vol world 0) ]
         [(key=? "=" key) (vol world 1) ]
+        [(key=? "," key) (temp world 0) ]
+        [(key=? "." key) (temp world 1) ]        
         [(key=? "escape" key) (exit) ]
         [(key=? "left" key) (Mchg world #f) ]
         [(key=? "right" key) (Mchg world #t) ]
@@ -364,7 +382,7 @@
         [(key=? "2" key) (andplay (ms-record? world) world)]
          
         [else world]
-)
+        )
   )
 
 
@@ -374,17 +392,14 @@
 (define (tock y)
 y
 )
-#|  (andplay (rs-scale  (ms-volume y) (rs-overlay* (list (scon y 0)(scon y 1)(scon y 2)(scon y 3)(scon y 4)(scon y 5)(scon y 6)(scon y 7)(scon y 8)(scon y 9)(scon y 10)(scon y 11) )))
-        (make-ms (ms-volume y) (ms-pressed? y) (ms-Plength y) (ms-octave y) (ms-kit y) (ms-record? y) (ms-recordlength y) (ms-rate y) (ms-tempo y)))
 
-|#
-
+;Midi is the big bang function that this program runs on.
+;WorldState -> Worldstate
 (define (Midi y)
     (big-bang y
               [on-tick tock 1]     
               [to-draw RENDER]
               [on-key keytracker]
-      
               )
     )
 (Midi test)
